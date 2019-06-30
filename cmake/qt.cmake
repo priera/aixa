2fe515e3a18c12c2fd7eafa8e82ae3e8c0f7bb7e
priera <mail@rieramartorell.cat>
2019-06-30 13:25:11 +0200

    Changed CMake structure

---
+++ cmake/qt.cmake
@@ -0,0 +1,9 @@
+set(Qt5_DIR /home/pedro/Qt/5.12.4/gcc_64/lib/cmake/Qt5)
+
+set(CMAKE_AUTOMOC ON)
+set(CMAKE_AUTOUIC ON)
+
+find_package(Qt5 COMPONENTS Core Gui Widgets REQUIRED)
+
+set(QT_INCLUDES ${Qt5Core_INCLUDE_DIRS} ${Qt5Gui_INCLUDE_DIRS} ${Qt5Widgets_INCLUDE_DIRS})
+set(QT_LIBS Qt5::Core Qt5::Gui Qt5::Widgets)
