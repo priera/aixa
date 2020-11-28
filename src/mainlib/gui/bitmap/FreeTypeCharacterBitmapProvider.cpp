a65283e31be41955d206946d9089737b9290132a
priera <mail@rieramartorell.cat>
2020-11-28 12:16:20 +0100

    glyphs cache

--- src/mainlib/gui/bitmap/FreeTypeCharacterBitmapProvider.cpp
+++ src/mainlib/gui/bitmap/FreeTypeCharacterBitmapProvider.cpp
@@ -1,45 +1,65 @@
 #include "FreeTypeCharacterBitmapProvider.h"
 
 #include <GL/gl.h>
+
 #include <stdexcept>
 
 FreeTypeCharacterBitmapProvider::FreeTypeCharacterBitmapProvider() {
     if (FT_Init_FreeType(&ft))
-        throw std::runtime_error("ERROR::FREETYPE: Could not init FreeType Library");
+        throw std::runtime_error(
+            "ERROR::FREETYPE: Could not init FreeType Library");
 
-    if (FT_New_Face(ft, "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf", 0, &face))
+    if (FT_New_Face(
+            ft,
+            "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf",
+            0, &face))
         throw std::runtime_error("ERROR::FREETYPE: Failed to load font");
 }
-
 
 FreeTypeCharacterBitmapProvider::~FreeTypeCharacterBitmapProvider() {
     FT_Done_Face(face);
     FT_Done_FreeType(ft);
 }
 
-Bitmap FreeTypeCharacterBitmapProvider::getCharacter(char c, unsigned int pixelSize) {
-    bool blankBuffer = (c == ' ');
-    char actualChar = (blankBuffer) ? 'N' : c;
+Bitmap FreeTypeCharacterBitmapProvider::getCharacter(char c,
+                                                     unsigned int pixelSize) {
+    const Bitmap *cached = cacheLookup(c, pixelSize);
+    if (cached != nullptr) {
+        return *cached;
+    } else {
+        return storeNewGlyph(c, pixelSize);
+    }
+}
 
+const Bitmap *FreeTypeCharacterBitmapProvider::cacheLookup(
+    char c, unsigned int pixelSize) {
+    const auto fontSizeIt = cache.find(pixelSize);
+    if (fontSizeIt == cache.end()) return nullptr;
+
+    const auto &charCache = fontSizeIt->second;
+    const auto charIt = charCache.find(c);
+    if (charIt == charCache.end()) return nullptr;
+
+    return &charIt->second;
+}
+
+Bitmap FreeTypeCharacterBitmapProvider::storeNewGlyph(char c,
+                                                      unsigned int pixelSize) {
     FT_Set_Pixel_Sizes(face, 0, pixelSize);
 
-    if (FT_Load_Char(face, actualChar, FT_LOAD_RENDER))
+    if (FT_Load_Char(face, c, FT_LOAD_RENDER))
         throw std::runtime_error("ERROR::FREETYTPE: Failed to load Glyph");
-
-
-    if (blankBuffer) {
-        for (int row = 0; row < face->glyph->bitmap.rows; row++) {
-            for (int col = 0; col < face->glyph->bitmap.width; col++) {
-                face->glyph->bitmap.buffer[row * face->glyph->bitmap.width + col] = 0x0F;
-            }
-        }
-    }
 
     auto buffer = face->glyph->bitmap.buffer;
     auto byteSize = face->glyph->bitmap.rows * face->glyph->bitmap.width;
 
-    return {face->glyph->bitmap.rows,
-            face->glyph->bitmap.width,
-            GL_RED,
-            std::vector<unsigned char>(buffer, buffer + byteSize)};
+    auto &metric = metricsStorage.emplace_back(face->glyph->bitmap_top,
+                                               face->glyph->bitmap_left,
+                                               face->glyph->advance.x);
+
+    const auto &built = cache[pixelSize].emplace(c, Bitmap {
+        face->glyph->bitmap.rows, face->glyph->bitmap.width, GL_RED,
+        std::vector<unsigned char>(buffer, buffer + byteSize), &metric});
+
+    return built.first->second;
 }
--- src/mainlib/gui/bitmap/FreeTypeCharacterBitmapProvider.h
+++ src/mainlib/gui/bitmap/FreeTypeCharacterBitmapProvider.h
@@ -2,21 +2,38 @@
 #define AIXA_FREETYPECHARACTERBITMAPPROVIDER_H
 
 #include <ft2build.h>
+
+#include <unordered_map>
 #include FT_FREETYPE_H
 
 #include "Bitmap.h"
 
 class FreeTypeCharacterBitmapProvider {
-public:
+   public:
+    struct GlyphMetrics {
+        GlyphMetrics(unsigned int top, unsigned int left, long advanceX)
+            : top(top), left(left), advanceX(advanceX) {}
+
+        unsigned int top, left, advanceX;
+    };
+
     FreeTypeCharacterBitmapProvider();
     virtual ~FreeTypeCharacterBitmapProvider();
 
     Bitmap getCharacter(char c, unsigned int pixelSize = 80);
 
-private:
+   private:
+    using GlyphCache = std::unordered_map<char, Bitmap>;
+    using FontSizeCache = std::unordered_map<unsigned int, GlyphCache>;
+
+    Bitmap storeNewGlyph(char c, unsigned int pixelSize);
+    const Bitmap *cacheLookup(char c, unsigned int pixelSize);
+
     FT_Library ft;
+
     FT_Face face;
+    FontSizeCache cache;
+    std::vector<GlyphMetrics> metricsStorage;
 };
 
-
-#endif //AIXA_FREETYPECHARACTERBITMAPPROVIDER_H
+#endif  // AIXA_FREETYPECHARACTERBITMAPPROVIDER_H
