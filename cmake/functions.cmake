3c9edb3e238a4c89752b3973bdea659239c4637e
priera <mail@rieramartorell.cat>
2021-05-05 12:33:22 +0200

    this is sad

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -1,6 +1,4 @@
 function(build_sources_list SRCS)
-    #Albert, si mai veus això: ets un màquina
-
     file(GLOB_RECURSE  SOURCEFILES CONFIGURE_DEPENDS *.cpp *.h *.cu *.cuh *.hpp *.ui *.qrc *.rc)
     #traverse unit test files and remove from sources
     file(GLOB_RECURSE SRCS_UTEST CONFIGURE_DEPENDS  *_UTest.* )
